904f6173d138493046f0c1c05ff5a6fb39875fb2
Vaishnav Prasad R K <88769806+rkvaishnavp@users.noreply.github.com>
2023-12-08 01:50:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import viteCNAMEPlugin from './viteCNAMEPlugin'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/rkvaishnavp.github.io/",
-  plugins: [react()],
+  plugins: [react(), viteCNAMEPlugin({ domain: 'rkvp.in' })]
 })
